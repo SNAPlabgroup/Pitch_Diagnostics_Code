b85e82857bf068f5f625c314342849de25e69f11
Andrew Sivaprakasam <andrew.n.sivaprakasam@gmail.com>
2023-07-12 17:36:24 -0400

    updating with chin RAM code. IN the works

---
+++ EFR_RAM/Chin/processChin.m
@@ -0,0 +1,94 @@
+%Author: Andrew Sivaprakasam
+%Updated: July, 2023
+%Purpose: Script to import/plot/apply additional processing to RAM_EFR
+%files (chin version)
+
+%TODO: 
+% - resample and truncate!
+
+close all
+clear;
+
+condition = 'Baseline';
+subj = 'Q421';
+fmod = 103;
+harmonics = 4;
+
+%% Handles my Local File Structure/EXT drive
+
+uname = 'sivaprakasaman';
+prefix = ['/media/',uname,'/AndrewNVME/Pitch_Study/Pitch_Diagnostics_SH_AS/EFR_RAM/Chin/'];
+suffix = [condition,'/',subj];
+datapath = [prefix,suffix];
+
+%% Import data
+cwd = pwd;
+cd(datapath)
+datafile = {dir(fullfile(cd,'p*RAM*.mat')).name};
+load(datafile{1});
+fname_out = [datafile{1}(1:end-4),'_matlab.mat'];
+cd(cwd);
+%% Data analysis & plotting:
+fs_orig = data.Stimuli.RPsamprate_Hz;
+fs = 8e3; %resample to 8kHz
+
+all_dat = cell2mat(data.AD_Data.AD_All_V');
+all_dat = all_dat';
+
+pos = all_dat(:,1:2:end)*1e6/data.AD_Data.Gain; %+ polarity
+neg = all_dat(:,1:2:end)*1e6/data.AD_Data.Gain; %- polarity
+
+% reample
+%% Get PLV spectra/Time domain waveform:
+
+%params for random sampling with replacement
+subset = 100;
+k_iters = 30;
+
+%only output things we want to look at
+[f, ~, ~, PLV_env, ~, ~, T_env] = helper.getSpectAverage(pos,neg, fs, subset, k_iters);
+t = (1:length(T_env))/fs;
+
+%% Get Peaks
+
+[PKS,LOCS] = helper.getPeaks(f,PLV_env,fmod,harmonics);
+
+%% Plot:
+blck = [0.25, 0.25, 0.25];
+rd = [0.8500, 0.3250, 0.0980, 0.5];
+figure;
+
+%Spectral Domain
+hold on;
+title([subj,' | RAM - 25% Duty Cycle'],'FontSize',14);
+plot(f,PLV_env,'Color',blck,'linewidth',1.5);
+plot(LOCS,PKS,'*','Color',rd,'MarkerSize',10,'LineWidth',2);
+
+hold off;
+ylim([0,1])
+ylabel('PLV','FontWeight','bold')
+xlabel('Frequency(Hz)','FontWeight','bold')
+
+%Time Domain
+xstart = .6;
+xend = .9;
+ystart = 0.6;
+yend = .9;
+
+axes('Position',[xstart ystart xend-xstart yend-ystart])
+box on
+hold on
+plot(t, T_env,'Color',blck, 'LineWidth',2);
+xlim([0.3,.4]);
+ylim([-1,1]);
+yticks([-1,0,1])
+xlabel('Time(s)','FontWeight','bold');
+ylabel('Amplitude \muV','FontWeight','bold')
+hold off
+
+set(gcf,'Position',[1557 538 560 420])
+
+%% Export:
+cd(datapath);
+print(gcf,[subj,'_RAM_efr_human',condition,'_figure'],'-dpng','-r300');
+cd(cwd)
