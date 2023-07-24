921caa6b626f98f092983dcac150389036c23102
Andrew Sivaprakasam <andrew.n.sivaprakasam@gmail.com>
2023-07-24 16:18:23 -0400

    changes to make the EFR_RAM python bdf preprocessing files more generalizable for user

--- EFR_RAM/Human/2_MatlabAnalysis/setup_AS.m
+++ EFR_RAM/Human/2_MatlabAnalysis/setup_AS.m
@@ -5,11 +5,13 @@ close all;
 clear;
 
 uname = 'asivapr';
+
+condition = 'YNH';
+subj = 'S360';
+
 prefix = ['/media/',uname,'/AndrewNVME/Pitch_Study/Pitch_Diagnostics_SH_AS/EFR_RAM/Human/'];
 suffix = [condition,'/',subj,'/Preprocessed'];
 datapath = [prefix,suffix];
 
-condition = 'YNH';
-subj = 'S362';
 
 processSubject;
