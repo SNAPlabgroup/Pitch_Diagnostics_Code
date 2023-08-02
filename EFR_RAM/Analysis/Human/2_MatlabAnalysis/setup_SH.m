0a7bea65ecf55aa80a0d9d89f0defe5a82393ca4
hausersn1 <samantha.n.hauser@gmail.com>
2023-08-01 22:52:03 -0400

    initial set up of SFOAE analysis

--- EFR_RAM/Analysis/Human/2_MatlabAnalysis/setup_SH.m
+++ EFR_RAM/Analysis/Human/2_MatlabAnalysis/setup_SH.m
@@ -9,7 +9,7 @@ uname = 'samhauser';
 condition = 'YNH';
 subj = 'S362';
 
-prefix = ['/Users/samhauser/Library/CloudStorage/Box-Box/SNAPlab Data Archive/Pitch_Diagnostics_SH_AS/EFR_RAM/Human/'];
+prefix = ['/Volume/SNH/THESIS/Pitch_Diagnostics_SH_AS/EFR_RAM/Human/'];
 suffix = [condition,'/',subj,'/Preprocessed'];
 datapath = [prefix,suffix];
 
---
+++ SFOAEswept/Analysis/Human/setup_SH.m
@@ -0,0 +1,18 @@
+%% Set up to run SFOAE analysis
+
+%Here's where you can define your own parameters for input/output
+%directories.
+
+close all;
+clear;
+
+subj = 'S362';
+condition = 'YNH';
+
+uname = 'samhauser';
+prefix = ['/Volumes/SNH/THESIS/Pitch_Diagnostics_Data/SFOAEswept/Human/'];
+suffix = [condition,'/',subj];
+datapath = [prefix,suffix];
+
+SFanalysis;
+
